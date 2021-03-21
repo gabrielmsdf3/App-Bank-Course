1f0cdb461ca4011db82d4551102e1fc2b1d61e9e
Gabriel Moura <gabrielmsdf3@gmail.com>
2021-03-21 11:50:23 -0300

    operacao saque encapsulada dentro da funcao

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 class Cliente {
   nome;
   cpf;
+}
+
+class ContaCorrente{
   agencia;
   saldo;
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
-
 cliente1.nome = "Gabriel";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
-cliente2.nome = "Larisse"
+const cliente2 = new Cliente();
+cliente2.nome = "Larisse";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteGabriel = new ContaCorrente();
+contaCorrenteGabriel.saldo = 0;
+contaCorrenteGabriel.agencia = 1001;
+
+console.log(contaCorrenteGabriel.saldo);
+contaCorrenteGabriel.saldo = 100;
+console.log(contaCorrenteGabriel.saldo);
+let valorSacado = 200;
+if(contaCorrenteGabriel.saldo >= valorSacado){
+  contaCorrenteGabriel.saldo -=valorSacado;
+}
+
+
+
+console.log(contaCorrenteGabriel.saldo);
+console.log(cliente1);
+console.log(cliente2);
