e60e9e68f7cb4b93765ceda2b86e5179ef0b6e0d
Gabriel Moura <gabrielmsdf3@gmail.com>
2021-03-25 22:36:13 -0300

    Interface autenticar

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ import {Diretor} from "./Funcionarios/Diretor.js"
 import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
 const diretor = new Diretor("Ícaro", 10000, 12345678900);
-diretor.cadastrarSenha("12345678")
+diretor.cadastrarSenha("123456")
 const gerente = new Gerente("Gabriel", 5000, 1234567898);
 gerente.cadastrarSenha("1234")
-const estaLogado = SistemaAutenticacao.login(gerente, "1234");
 
-console.log(estaLogado);
+const cliente = new Cliente("Laís", 29008767891, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "1234");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+
+console.log(clienteEstaLogado);
+console.log(gerenteEstaLogado);
+console.log(diretorEstaLogado);
 
 
